ed197b07e74849424069a80023ae33cc94df9c4e
Cleo Gao <celeo127@outlook.com>
2022-08-03 16:48:38 +0800

    Implemented dark/light themes

---
+++ component/Button/Button.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Button } from "./Button";
+import userEvent from '@testing-library/user-event';
+
+
+describe("Button test cases", ()=> {
+    it("Render check", () => {
+        const onClick = jest.fn();
+        const { asFragment } =render(<Button onClick={onClick}>Button</Button>);
+
+        expect(asFragment()).toMatchSnapshot();
+        
+});
+
+    it("Check onClick callback", async () => {
+        const onClick = jest.fn();
+        
+        render(<Button onClick={onClick}>Button</Button>);
+
+        screen.debug();
+
+        const element = screen.getByRole("button");
+
+        await userEvent.click(element);
+
+        expect(onClick).toHaveBeenCalled();
+    });
+});
+
+
+
+// const onClick = jest.fn();
+//         const { asFragment } =render(<Button onClick={onClick}>Button</Button>);
+
+//         expect(asFragment()).toMatchSnapshot();
+//     });
+
+//     it("Check onClick callback", () => {
+//         const onClick = jest.fn();
+
+//         render(<Button onClick={onClick}>Button</Button>);
+
+//         //screen.debug();
+//         const element = screen.getByRole("button");
+
+//         userEvent.click(element);
+
+//         expect(onClick).toHaveBeenCalled();
+
+        
+//     });
